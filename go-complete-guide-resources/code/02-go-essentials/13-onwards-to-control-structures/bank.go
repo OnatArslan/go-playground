0a35da12288c9057dd301dab66d38a664835dfdd
OnatArslan <onatarslan13@gmail.com>
2025-12-02 22:32:36 +0300

    Commit for changes

--- go-complete-guide-resources/code/02-go-essentials/13-onwards-to-control-structures/bank.go
+++ go-complete-guide-resources/code/02-go-essentials/13-onwards-to-control-structures/bank.go
@@ -1,18 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Welcome to Go Bank!")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
+	accountBalance := 1000.0
+	for {
+		fmt.Println("Welcome to Go Bank!")
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
 
-	var choice int
-	fmt.Print("Your choice: ")
-	fmt.Scan(&choice)
+		var choice int
 
-	fmt.Println("Your choice:", choice)
+		fmt.Println("Waiting for your choice")
+		fmt.Scan(&choice)
+
+		fmt.Println("Your choice is:", choice)
+
+		if choice == 1 {
+			fmt.Printf("Your balance is %.2f\n", accountBalance)
+		} else if choice == 2 {
+
+			var amount float64
+			fmt.Println("Please enter an amount for deposit")
+			fmt.Scan(&amount)
+
+			if amount >= 0 {
+				accountBalance += amount
+				fmt.Printf("Your new balance is %.2f\n", accountBalance)
+			} else {
+				fmt.Println("Negative amount given")
+				break
+			}
+		} else if choice == 3 {
+
+			var amount float64
+			fmt.Println("Please enter an amount for withdraw")
+			fmt.Scan(&amount)
+			if accountBalance >= amount {
+				accountBalance -= amount
+
+				fmt.Printf("Your new account balance is %.2f", accountBalance)
+			} else {
+				fmt.Println("You don't have that kind of money")
+				break
+			}
+		} else {
+			break
+		}
+	}
+
 }
